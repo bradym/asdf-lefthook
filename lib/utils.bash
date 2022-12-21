c07c2b00c0ffdd0ba1b05b908a5fc72775454c8a
Brady Mitchell <bradym@users.noreply.github.com>
2022-12-21 11:30:18 -0700

    install works

--- lib/utils.bash
+++ lib/utils.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+set -x
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for lefthook.
 GH_REPO="https://github.com/evilmartians/lefthook"
 TOOL_NAME="lefthook"
 TOOL_TEST="lefthook version"
@@ -14,7 +14,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if lefthook is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -27,22 +26,42 @@ sort_versions() {
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    sed 's/^v//'
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if lefthook has other means of determining installable versions.
   list_github_tags
 }
 
+get_platform() {
+  case "$(uname -s)" in
+    Linux)
+      echo "Linux"
+    ;;
+    Darwin)
+      echo "MacOS"
+    ;;
+  esac
+}
+
 download_release() {
-  local version filename url
+  local version filename url platform arch
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for lefthook
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  platform=$(uname -s)
+
+  if [[ "$platform" == "Darwin" ]]; then
+    platform="MacOS"
+  fi
+
+  arch=$(uname -m)
+
+  url="$GH_REPO/releases/download/v$version/lefthook_${version}_${platform}_${arch}"
+
+  if [[ -f "$filename" ]]; then
+    rm "$filename"
+  fi
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,9 +80,10 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Assert lefthook executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+
+    chmod +x "$install_path/$tool_cmd"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
     echo "$TOOL_NAME $version installation was successful!"
